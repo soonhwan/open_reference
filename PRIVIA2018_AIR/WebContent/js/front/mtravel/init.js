35082705979b0b963857b43188213493091db190
shkwon <soonhwan1237@gmail.com>
2018-12-17 21:23:16 +0900

    모바일 항공 개편 시작

---
+++ PRIVIA2018_AIR/WebContent/js/front/mtravel/init.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+$(document).bind('mobileinit',function(){
+	$.mobile.keepNative = "a, button, select, input, textarea";
+	$.mobile.defaultPageTransition = "slide";
+	$.mobile.defaultDialogTransition = "slideup";
+});
