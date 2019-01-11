a36b04dff3ee988aadbc16653c9f8d05fae0a773
shkwon <soonhwan1237@gmail.com>
2019-01-11 15:33:18 +0900

    no message

---
+++ codingmap/public/javascripts/issue2_worker.js
@@ -0,0 +1,14 @@
+var lastIssueList = [];
+function issueRequest(){
+    var url = 'https://redmine.tidesquare.com/issues.json?assigned_to_id=300&jsonp=callbackFunc'
+    importScripts(url);
+}
+
+function callbackFunc(res){
+    //console.log(res)
+    postMessage(res);
+    lastIssueList = res.issues;
+    setTimeout('issueRequest()',10000) //10sec refesh
+}
+
+issueRequest();
---
+++ codingmap/public/javascripts/myip.js
@@ -0,0 +1,1 @@
+var myip = "http://codingmap.tidesquare.com";
