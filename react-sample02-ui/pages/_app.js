2a2ec1ab969b183479f7b68331948b64de27daa3
soonhwan <soonhwan1237@gmail.com>
2021-12-15 09:46:16 +0900

    no message

--- react-sample02-ui/pages/_app.js
+++ react-sample02-ui/pages/_app.js
@@ -15,7 +15,6 @@ const NextProject = ({ Component }) => {
         <title>NEXT PROJECT</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       </Head>
       <Component />
     </>
